768cf60ddb0dd4b2d38a728865d4fd193919d308
Floriane HJ <fjulia.dev@gmail.com>
2024-09-30 12:05:33 +0200

    ajout navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+//import icons
+import { BiHomeAlt, BiUser } from 'react-icons/bi';
+import { BsBriefcase, BsClipboardData } from 'react-icons/bs';
+
+//link
+import { Link } from 'react-scroll';
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return <nav className='fixed bottom-2 lg:bottom:8 w-full overflow-hidden z-50'>
+    <div className="container mx-auto">
+      { /* nav inner */}
+      <div className='w-full bg-black/20 h-[75px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2x1 text-white/50 '>
+        <Link to='home' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+          <BiHomeAlt />
+        </Link>
+        <Link to='about' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+          <BiUser />
+        </Link>
+        <Link to='services' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+          <BsClipboardData />
+        </Link>
+        <Link to='work' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+          <BsBriefcase />
+        </Link>
+        <Link to='contact' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+          <BsBriefcase />
+        </Link>
+      </div>
+    </div>
+  </nav>
 };
 
 export default Nav;
