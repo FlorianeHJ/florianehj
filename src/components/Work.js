31b4b00b9254a52952911ab77a1207ca342a5a50
Floriane HJ <fjulia.dev@gmail.com>
2024-10-01 17:55:36 +0200

    ajout plusieurs cardes

--- src/components/Work.js
+++ src/components/Work.js
@@ -54,7 +54,54 @@ const Work = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="flex-1">2</div>
+                    <div className="flex-1 flex flex-col gap-y-10">
+                        {/* {images} */}
+                        <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                            {/* {overlay} */}
+                            <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                            {/* {images} */}
+                            <img
+                                className="group-hover:scale-125 transition-all duration-500"
+                                src={Img1}
+                                alt=""
+                            />
+                            {/* {pretitle} */}
+                            <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                                <span className="text-gradient">
+                                    UI/UX Design
+                                </span>
+                            </div>
+                            {/* {title} */}
+                            <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                                <span className="text-3x1 text-white">
+                                    Project Title
+                                </span>
+                            </div>
+                        </div>
+                        {/* {images} */}
+                        <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                            {/* {overlay} */}
+                            <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                            {/* {images} */}
+                            <img
+                                className="group-hover:scale-125 transition-all duration-500"
+                                src={Img1}
+                                alt=""
+                            />
+                            {/* {pretitle} */}
+                            <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                                <span className="text-gradient">
+                                    UI/UX Design
+                                </span>
+                            </div>
+                            {/* {title} */}
+                            <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                                <span className="text-3x1 text-white">
+                                    Project Title
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
