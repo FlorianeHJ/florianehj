35d205d80a16cda834db34c5ef95233bbaa7bee4
Floriane HJ <fjulia.dev@gmail.com>
2024-10-01 15:26:58 +0200

    ajout about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,69 @@
 import React from 'react';
 
+import CountUp from 'react-countup';
+
+import { useInView } from "react-intersection-observer";
+
+import { fadeIn } from "../variants";
+
+import { motion } from 'framer-motion';
+
 const About = () => {
-  return <div id='about' className='section'>About</div>;
+
+  const [ref, inView] = useInView ({
+    threshold: 0.5,
+  })
+
+  return ( 
+    <section id='about' className='section' ref={ref}>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h:screen'>
+          {/* {img} */}
+          <motion.div 
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3}}
+            className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'></motion.div>
+          {/* {text} */}
+          <div className='flex-1'>
+            <h2 className='h2 text-accent'>About me</h2>
+            <h3 className='h3 mb-4'>I'm a Front-end Developer with expertise in React.</h3>
+            <p className='mb-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, ut. Obcaecati provident ea itaque. Vitae sed, dolorem eaque, sit reprehenderit quo voluptas similique accusantium nihil tempore inventore sapiente reiciendis voluptate.</p>
+            {/* {stats} */}
+            <div className='flex gap-x-6 lg:gap-x-10 mb-12'> 
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2' >
+                  {
+                    inView ? <CountUp start={0} end={13} duration={3} />: null }
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>Years of <br /> Experience</div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2' >
+                  {
+                    inView ? <CountUp start={0} end={15} duration={3} />: null }
+                    k+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>Projets <br /> Completed</div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2' >
+                  {
+                    inView ? <CountUp start={0} end={12} duration={3} />: null }
+                    k+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>Satisfied <br /> Clients</div>
+              </div>
+            </div>
+            <div className='flex gap-x-8 items-center'>
+              <button className='btn btn-lg'>Contact me</button>
+              <a href="#" className='text-gradient btn-link'>My Portfolio</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> );
 };
 
 export default About;
