6462419cf495594a9db8eb85196e5aa25dc51050
Floriane HJ <fjulia.dev@gmail.com>
2024-10-02 10:31:11 +0200

    partie work terminée

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,25 +1,31 @@
 import React from 'react'
 
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'
 
-// import { fadeIn } from "../variants"
+import { fadeIn } from '../variants'
 
 import Img1 from '../assets/portfolio-img1.png'
-// import Img2 from "../assets/portfolio-img2.png"
-// import Img3 from "../assets/portfolio-img3.png"
+import Img2 from '../assets/portfolio-img2.png'
+import Img3 from '../assets/portfolio-img3.png'
 
 const Work = () => {
     return (
         <section id="work" className="section">
             <div className="container mx-auto">
                 <div className="flex flex-col lg:flex-row gap-x-10">
-                    <div className=" flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+                    <motion.div
+                        variants={fadeIn('right', 0.3)}
+                        initial="hidden"
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className=" flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
+                    >
                         {/* {text} */}
                         <div>
                             <h2 className="h2 leading-tight text-accent">
                                 My latest <br /> Work
                             </h2>
-                            <p className="max-w-sm mb-16">
+                            <p className="max-w-sm">
                                 Lorem ipsum dolor sit, amet consectetur
                                 adipisicing elit. Fuga tempora omnis vitae alias
                                 labore nam repudiandae assumenda modi facere
@@ -53,16 +59,22 @@ const Work = () => {
                                 </span>
                             </div>
                         </div>
-                    </div>
-                    <div className="flex-1 flex flex-col gap-y-10">
-                        {/* {images} */}
+                    </motion.div>
+                    <motion.div
+                        variants={fadeIn('left', 0.2)}
+                        initial="hidden"
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className="flex-1 flex flex-col gap-y-10"
+                    >
+                        {/* {CARD} */}
                         <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
                             {/* {overlay} */}
                             <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
                             {/* {images} */}
                             <img
                                 className="group-hover:scale-125 transition-all duration-500"
-                                src={Img1}
+                                src={Img2}
                                 alt=""
                             />
                             {/* {pretitle} */}
@@ -78,14 +90,14 @@ const Work = () => {
                                 </span>
                             </div>
                         </div>
-                        {/* {images} */}
+                        {/* {CARD} */}
                         <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
                             {/* {overlay} */}
                             <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
                             {/* {images} */}
                             <img
                                 className="group-hover:scale-125 transition-all duration-500"
-                                src={Img1}
+                                src={Img3}
                                 alt=""
                             />
                             {/* {pretitle} */}
@@ -101,7 +113,7 @@ const Work = () => {
                                 </span>
                             </div>
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
