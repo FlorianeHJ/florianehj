1d797dea467dde69a3124be5724e58cae1130b65
Floriane HJ <fjulia.dev@gmail.com>
2024-10-02 11:16:25 +0200

    version 1.0 terminée

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,7 +27,13 @@ const Contact = () => {
                         </div>
                     </motion.div>
                     {/* {form} */}
-                    <form className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start">
+                    <motion.form
+                        variants={fadeIn('left', 0.3)}
+                        initial="hidden"
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start"
+                    >
                         <input
                             className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
                             type="text"
@@ -43,7 +49,7 @@ const Contact = () => {
                             placeholder="Your message"
                         ></textarea>
                         <button className="btn btn-lg">Send Message</button>
-                    </form>
+                    </motion.form>
                 </div>
             </div>
         </section>
